1575e10647312f6798735af819d819598ff5fd2b
Magesh <tkmagesh77@gmail.com>
2018-04-05 15:59:52 +0530

    Day-01 complete

---
+++ 02-Closures/spinner.js
@@ -0,0 +1,31 @@
+var spinner = ...
+
+spinner.up() // => 1
+spinner.up() // => 2
+spinner.up() // => 3
+spinner.up() // => 4
+
+spinner.down() //=> 3
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+
+var spinner = (function(){
+	var counter = 0;
+
+
+	function increment(){
+		return ++counter;
+	}
+
+	function decrement(){
+		return --counter;
+	}
+
+	return {
+		up : increment,
+		down : decrement
+	};
+})();
