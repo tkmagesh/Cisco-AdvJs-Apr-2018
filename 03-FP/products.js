761e0b1309915a7e7ba9aaaa011450a6ea7bf7e8
Magesh <tkmagesh77@gmail.com>
2018-04-06 11:00:52 +0530

    fp assignment

---
+++ 03-FP/products.js
@@ -0,0 +1,9 @@
+var products = [
+	{id : 6, name : 'Pen', cost : 50, units : 20, category : 'stationary'},
+	{id : 9, name : 'Ten', cost : 70, units : 70, category : 'stationary'},
+	{id : 3, name : 'Len', cost : 60, units : 60, category : 'grocery'},
+	{id : 5, name : 'Zen', cost : 30, units : 30, category : 'grocery'},
+	{id : 1, name : 'Ken', cost : 20, units : 80, category : 'utencil'},
+];
+
+//find the sum of product value (cost * units) of stationary products (use filter, map * reduce)
